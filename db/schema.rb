49093a2becc27459fa60cb1b60fd7618bb5d0a66
Aram <aramhrptn@hotmail.com>
2024-11-29 23:48:39 +0400

    Add URLs table with unique slug index to schema
    This commit introduces a new "urls" table with a UUID primary key generated by the "gen_random_uuid()" function. The table includes "slug" and "original" columns, both of type string, and timestamps for creation and updates. An index is added on the "slug" column to ensure uniqueness.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_29_192204) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_29_194642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "urls", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "slug"
+    t.string "original"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_urls_on_slug", unique: true
+  end
 end
