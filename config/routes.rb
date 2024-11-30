767ee8d7e20d1f9f10ce84efca11863e607baca7
Aram <aramhrptn@hotmail.com>
2024-11-30 17:49:54 +0400

    Add named route for slug redirect in home controller
    This change introduces a named route, :slug_redirect, for the existing slug path, enhancing code readability and maintainability. By giving the route a name, it can be referenced more conveniently throughout the codebase, improving clarity and consistency in URL handling.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   root to: "home#index"
-  get "/:slug", to: "home#show"
+  get "/:slug", to: "home#show", as: :slug_redirect
 end
