1e396e647a18fd039ed31758b0a76a4b89600c0c
Aram <aramhrptn@hotmail.com>
2024-11-30 14:57:54 +0400

    Enforce URL uniqueness and validation.
    Add validation to ensure the uniqueness of slugs and presence of the original URL, including a format check using URI regex. Implement a callback to generate a 6-character alphanumeric slug before creating URL records.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,12 @@
 class Url < ApplicationRecord
+  validates_uniqueness_of :slug
+  validates_presence_of :original
+  validates :original, format: { with: URI.regexp }
+
+  before_create :generate_slug
+
+  private
+  def generate_slug
+    self.slug = SecureRandom.alphanumeric(6)
+  end
 end
