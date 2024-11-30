728513303239333c7471825b892ad9ae239c4b9e
Aram <aramhrptn@hotmail.com>
2024-11-30 18:01:31 +0400

    Format route resources for URL shortener
    Adjust spacing around resource actions in the routes definition for consistency. This change aims to improve code readability without altering functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :shortner do
     namespace :simple do
-      resources :urls, only: [:show, :new, :create]
+      resources :urls, only: [ :show, :new, :create ]
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
