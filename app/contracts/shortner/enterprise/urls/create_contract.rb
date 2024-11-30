73fb43529e0738e1313f6d13bf1c9995f6fd7eb8
Aram <aramhrptn@hotmail.com>
2024-11-30 18:25:46 +0400

    Enforce slug validation in URL creation contract
    Add slug validation rules to ensure only alphanumeric characters are allowed and to check if the slug has already been taken. This change enhances data integrity by preventing duplicate or invalid slugs in the URL creation process.

--- app/contracts/shortner/enterprise/urls/create_contract.rb
+++ app/contracts/shortner/enterprise/urls/create_contract.rb
@@ -5,4 +5,11 @@ class Shortner::Enterprise::Urls::CreateContract < ActiveTransaction::Applicatio
     optional(:slug).maybe(:string)
     required(:original).filled(:string)
   end
+
+  rule(:slug) do
+    if value.present?
+      key.failure("must be alphanumeric") unless value.match?(/\A[a-zA-Z0-9]+\z/)
+      key.failure("has already been taken") if Url.exists?(slug: value)
+    end
+  end
 end
