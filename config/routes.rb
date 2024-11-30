82e68f4fb3086d19497f5a3524a31c5feb4e8944
Aram <aramhrptn@hotmail.com>
2024-11-30 17:16:50 +0400

    Add URL shortener routes under simple shortner namespace
    This commit introduces a new namespace for URL shortener functionality, specifically under 'shortner/simple', and defines resourceful routes for 'urls', excluding the index action. This restructuring aims to organize related URL shortening features, facilitating better code management and future expansions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :shortner do
+    namespace :simple do
+      resources :urls, except: :index
+    end
+  end
   get "up" => "rails/health#show", as: :rails_health_check
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
